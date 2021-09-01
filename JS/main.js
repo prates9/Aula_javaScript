7bb99938c981adab292f9aba041033ff04bde239
Carlos Prates <89169495+prates9@users.noreply.github.com>
2021-08-31 21:24:07 -0300

    aula com java

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+
+var name = "Carlos Prates";
+var n1 = 29;
+var n2 = 10;
+var frase ="Muito interessante estou gostando";
+//alert(nome + "tem" + idade + "anos");
+//alert (idade +idade2)
+console.log(n1 * n2);
+console.log(frase.replace("interessante","divertido"));
+//alert (frase.replace("interessante","divertido"))
+console.log(frase.toLowerCase())
